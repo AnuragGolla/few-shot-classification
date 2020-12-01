ed3df73db613487192d0b9397cfd7c7718e93057
Justin Xiang <jxiang@Justins-MacBook-Pro.local>
2020-12-01 15:16:05 -0500

    m

--- utils/distances/earth_mover.py
+++ utils/distances/earth_mover.py
@@ -12,8 +12,8 @@ def emd_distance(support, support_mean, query):
     support_mean = support_mean.unsqueeze(2).expand(-1, -1, 5, -1) # 32, 5, 5, 64
     query = query.unsqueeze(1).expand(-1, 5, -1, -1) # 32, 5, 5, 64
 
-    support_mean = support_mean.view(B*way*way, 1, Depth) # 800, 64
-    query = query.view(B*way*way, 1, Depth) # 800, 64
+    support_mean = support_mean.contiguous().view(B*way*way, 1, Depth) # 800, 64
+    query = query.contiguous().view(B*way*way, 1, Depth) # 800, 64
 
     dist = earth_mover_distance(support_mean.cuda(), query.cuda(), transpose=False)
 
