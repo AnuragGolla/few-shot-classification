1146669af3f4f37549542ffb59988197d7eb8e73
Justin Xiang <jxiang@Justins-MacBook-Pro.local>
2020-12-01 15:13:23 -0500

    m

--- utils/distances/earth_mover.py
+++ utils/distances/earth_mover.py
@@ -7,6 +7,18 @@ from PyTorchEMD.emd import earth_mover_distance
 import pdb
 
 def emd_distance(support, support_mean, query):
+    B, way, shot, Depth = support.shape
+
+    support_mean = support_mean.unsqueeze(2).expand(-1, -1, 5, -1) # 32, 5, 5, 64
+    query = query_mean.unsqueeze(1).expand(-1, 5, -1, -1) # 32, 5, 5, 64
+
+    support_mean = support_mean.view(B*way*way, 1, Depth) # 800, 64
+    query = query.view(B*way*way, 1, Depth) # 800, 64
+
+    dist = earth_mover_distance(support_mean.cuda(), query.cuda(), transpose=False)
+
+    dist = dist.view(B, way, way)
+
     pdb.set_trace()
     ret = nnt.metrics.emd_loss(support_mean, query)
     print(ret.shape)
