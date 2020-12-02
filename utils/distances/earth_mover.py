567941c7ef13e1c913ae956c886c88969327b6c9
Justin Xiang <jxiang@Justins-MacBook-Pro.local>
2020-12-01 23:38:35 -0500

    m

--- utils/distances/earth_mover.py
+++ utils/distances/earth_mover.py
@@ -3,6 +3,7 @@ import numpy as np
 import ot
 import neuralnet_pytorch as nnt 
 from PyTorchEMD.emd import earth_mover_distance 
+from neuralnet_pytorch.metrics import emd_loss
 
 import pdb
 
@@ -20,12 +21,17 @@ def emd_distance(support, support_mean, query):
     support_mean_normalized = support_mean_normalized.unsqueeze(1).repeat(1,5,1,1) # 32, 5, 5, 64 # abc abc abc
     query_normalized = query_normalized.unsqueeze(2).repeat_interleave(5, dim=2) # 32, 5, 5, 64  # aaa bbb ccc 
 
+    pdb.set_trace()
 
-    support_mean_normalized = support_mean_normalized.contiguous().view(B * way * way, 1, Depth) # 32, 25 64
-    query_normalized = query_normalized.contiguous().view(B * way * way, 1, Depth) # 32, 25, 64
+    support_mean_normalized = support_mean_normalized.contiguous().view(B * way * way, 1, Depth) # 800, 1 64
+    query_normalized = query_normalized.contiguous().view(B * way * way, 1, Depth) # 800, 1 64
 
-    dist = earth_mover_distance(support_mean_normalized, query_normalized, transpose=False)
+    pdb.set_trace()
 
-    dist = dist.view(B, way, way)
+    # dist = earth_mover_distance(support_mean_normalized, query_normalized, transpose=False)
 
-    return -dist
+    # dist = dist.view(B, way, way)
+
+    # return -dist
+
+    print("DASDSA")
